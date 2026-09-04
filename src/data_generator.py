c32e131ac2f4055e7da8afb71408595fac805b99
leoamullins <leomullins18@gmail.com>
2026-09-04 17:26:18 +0100

    improved the sampling and data generation methods.

--- src/data_generator.py
+++ src/data_generator.py
@@ -1,31 +1,30 @@
 import numpy as np
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF
+from scipy.linalg import cholesky
+import scipy
 
 
-def generate_data(n_samples, m_sensors, n_queries):
-    kernel = RBF(length_scale=0.2)
-    gp = GaussianProcessRegressor(kernel=kernel)
+def build_cholesky_factor(x_fine, ell, jitter=1e-8):
+    n_fine = x_fine.shape[0]
 
-    sensors = np.linspace(0, 1, m_sensors)
+    # use broadcasting to make pairwise distance matrix
+    D2 = (x_fine[:, None] - x_fine[None, :]) ** 2
 
-    u_data = []  # branch inputs
-    x_data = []  # trunk inputs
-    s_data = []  # targets
+    # calculate the kernel
+    K = np.exp(-D2 / (2 * ell**2))
 
-    for _ in range(n_samples):
-        # sample random input function
-        u = gp.sample_y(sensors.reshape(-1, 1), n_samples=1).flatten()
+    # adding the jitter to avoid, lin alg error
+    K += jitter * np.eye(n_fine)
+    return cholesky(K, lower=True)
 
-        # random point
-        x_query = np.random.uniform(0, 1)
 
-        # compute target int from 0 to x_query
-        mask = sensors <= x_query
-        s = np.trapezoid(u[mask], sensors[mask])
+def sample_functions(L, n_functions, rng):
+    n = L.shape[0]
+    z = rng.standard_normal((n, n_functions))
 
-        u_data.append(u)
-        x_data.append([x_query])
-        s_data.append([s])
+    # transform z with cholesky factor L
+    u = L @ z
+    return u.T
 
-    return u_data, x_data, s_data
+
+def compute_antiderivative(u, x_fine):
+    return scipy.integrate.cumulative_trapezoid(u, x_fine, axis=1, initial=0)
